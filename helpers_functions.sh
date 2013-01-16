31242c2f2aa4638b06ca0c1694493754c96628af
Алексей Павлов <alexey.pawlow@gmail.com>
2013-01-16 09:34:02 +0400

    Check if we move toolchain correctly

--- helpers_functions.sh
+++ helpers_functions.sh
@@ -67,13 +67,31 @@ toolchains_prepare() {
 		local _ext_mingw32=$(get_filename_extension $_file_mingw32)
 		func_download mingw32 ".$_ext_mingw32" $URL_MINGW32
 		func_uncompress ${_file_mingw32%.$_ext_mingw32} ".$_ext_mingw32" $TOOLCHAINS_DIR
-		mv $TOOLCHAINS_DIR/mingw $TOOLCHAINS_DIR/mingw32
+		
+		if [ -f $TOOLCHAINS_DIR/mingw32-move.marker ]
+		then
+			echo "--> Moved"
+		else
+			echo -n "--> Move $TOOLCHAINS_DIR/mingw to $TOOLCHAINS_DIR/mingw32..."
+			mv $TOOLCHAINS_DIR/mingw $TOOLCHAINS_DIR/mingw32 || die "Cannot move mingw->mingw32"
+			echo " done"
+			touch $TOOLCHAINS_DIR/mingw32-move.marker
+		fi
 		
 		local _file_mingw64=$(basename $URL_MINGW64)
 		local _ext_mingw64=$(get_filename_extension $_file_mingw64)
 		func_download mingw64 ".$_ext_mingw64" $URL_MINGW64
 		func_uncompress ${_file_mingw64%.$_ext_mingw64} ".$_ext_mingw64" $TOOLCHAINS_DIR
-		mv $TOOLCHAINS_DIR/mingw $TOOLCHAINS_DIR/mingw64
+		
+		if [ -f $TOOLCHAINS_DIR/mingw64-move.marker ]
+		then
+			echo "--> Moved"
+		else
+			echo -n "--> Move $TOOLCHAINS_DIR/mingw to $TOOLCHAINS_DIR/mingw64..."
+			mv $TOOLCHAINS_DIR/mingw $TOOLCHAINS_DIR/mingw64 || die "Cannot move mingw->mingw64"
+			echo " done"
+			touch $TOOLCHAINS_DIR/mingw64-move.marker
+		fi
 		
 		echo "--> Preparing done"
 	fi
