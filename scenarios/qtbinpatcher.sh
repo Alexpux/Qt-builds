f4ff99a64617ef1c15abc5d682e16ca46e559a91
Alexpux <alexey.pawlow@gmail.com>
2013-04-08 18:39:51 +0400

    Need copy qtbinpatcher for every Qt version.

--- scenarios/qtbinpatcher.sh
+++ scenarios/qtbinpatcher.sh
@@ -78,9 +78,9 @@ pkg_build() {
 }
 
 pkg_install() {
-	if [ ! -f $BUILD_DIR/$P/install.marker ]
+	if [ ! -f $BUILD_DIR/$P/install-${QT_VERSION}.marker ]
 	then
 		cp -f $BUILD_DIR/$P/out/${P}.exe ${QTDIR}/ || die "Error copying ${P}.exe"
-		touch $BUILD_DIR/$P/install.marker
+		touch $BUILD_DIR/$P/install-${QT_VERSION}.marker
 	fi
 }
