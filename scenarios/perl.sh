49e81f97cd126c57c65c6bb92e2bb2bd7c9852d6
Alexpux <alexey.pawlow@gmail.com>
2013-09-09 08:29:21 +0400

    Perl script. Add license info, fix conditions.

--- scenarios/perl.sh
+++ scenarios/perl.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
-set -e
 
-P=perl
+#
+# The BSD 3-Clause License. http://www.opensource.org/licenses/BSD-3-Clause
+#
+# This file is part of 'Qt-builds' project.
+# Copyright (c) 2013 by Alexpux (alexpux@gmail.com)
+# All rights reserved.
+#
+# Project: Qt-builds ( https://github.com/Alexpux/Qt-builds )
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# - Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the distribution.
+# - Neither the name of the 'Qt-builds' nor the names of its contributors may
+#     be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# **************************************************************************
+
+P="perl"
 P_V=${P}-${PERL_VERSION}
 EXT=".tar.gz"
 SRC_FILE="${P_V}${EXT}"
@@ -40,10 +74,9 @@ src_patch() {
 src_configure() {
 	lndirs
 
-	if [ -f $BUILD_DIR/$P_V/win32/configure.marker ]
-	then
+	[[ -f $BUILD_DIR/$P_V/win32/configure.marker ]] && {
 		echo "---> configured"
-	else
+	} || {
 		pushd $BUILD_DIR/$P_V/win32 > /dev/null
 		echo -n "---> configure..."
 		
@@ -72,15 +105,14 @@ src_configure() {
 		echo " done"
 		touch configure.marker
 		popd > /dev/null
-	fi
+	}
 }
 
 pkg_build() {
 
-	if [ -f $BUILD_DIR/$P_V/win32/make.marker ]
-	then
+	[[ -f $BUILD_DIR/$P_V/win32/make.marker ]] && {
 		echo "---> Builded"
-	else
+	} || {
 		pushd $BUILD_DIR/$P_V/win32 > /dev/null
 		echo "---> Building..."
 		change_paths
@@ -89,15 +121,14 @@ pkg_build() {
 		echo " done"
 		touch make.marker
 		popd > /dev/null
-	fi
+	}
 }
 
 pkg_install() {
 
-	if [ -f $BUILD_DIR/$P_V/win32/install.marker ]
-	then
+	[[ -f $BUILD_DIR/$P_V/win32/install.marker ]] && {
 		echo "---> Installed"
-	else
+	} || {
 		pushd $BUILD_DIR/$P_V/win32 > /dev/null
 		echo "---> Installing..."
 		change_paths
@@ -106,5 +137,5 @@ pkg_install() {
 		echo " done"
 		touch install.marker
 		popd > /dev/null
-	fi
+	}
 }
