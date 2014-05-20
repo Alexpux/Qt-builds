e8f8b72b1576dcbaacb020c84538ff4b23769203
Alexpux <alexey.pawlow@gmail.com>
2014-05-20 08:51:38 +0400

    libxml2: Update patch set.

--- scenarios/libxml2.sh
+++ scenarios/libxml2.sh
@@ -54,11 +54,29 @@ src_unpack() {
 
 src_patch() {
 	local _patches=(
-		$P/libxml2-2.9.1-disable_static_modules.patch
-		$P/libxml2-2.8.0_rc1-winnt.patch
 		$P/libxml2-2.9.0-largefile.patch
 		$P/libxml2-2.9.0-man_fixes.patch
 		$P/libxml2-2.9.0-open.gz.patch
+		$P/0004-detect-ws2tcpip.mingw.patch
+		$P/0005-fix-char-cast-warning.mingw.patch
+		$P/0006-fix-unused-var-warning.mingw.patch
+		$P/0007-fix-stat-redefinition.mingw.patch
+		$P/0008-include-winsock2.h-before-windows.h.mingw.patch
+		$P/0009-use-wstat-appropriate-for-mingw-w64.mingw.patch
+		$P/0010-mingw-w64-defines-lots-of-errnos.mingw.patch
+		$P/0011-more-winsock-inclusion-order-fixes.mingw.patch
+		$P/0012-socklen-is-signed-on-mingw-w64.mingw.patch
+		$P/0013-fix-field-type-signedness.all.patch
+		$P/0014-fix-prototype-warning.all.patch
+		$P/0015-fix-unused-parameters-warning.all.patch
+		$P/0016-WARNING-to-be-fixed.all.patch
+		$P/0017-fix-const-warning.all.patch
+		$P/0018-function-declaration-isnt-prototype.all.patch
+		$P/0019-unused-flags.all.patch
+		$P/0020-fix-size_t-format-specifier.all.patch
+		$P/0023-fix-sitedir-detection.mingw.patch
+		$P/0024-shrext-pyd.mingw.patch
+		$P/0025-mingw-python-dont-remove-dot.patch
 	)
 	
 	func_apply_patches \
